f165280067ffa17d977d2d5bbed36c6ff032ec87
jonyejin <i3pivigui@icloud.com>
2023-02-08 13:45:19 +0900

    added shell for home ubuntu

---
+++ home_ubuntu.sh
@@ -0,0 +1,16 @@
+
+#mount
+#sudo mount_smbfs -d 777 -f 777 //yejin:Qwer1234@165.194.27.114/DI-Works/프로젝트/뉴스 프로젝트/데이터/뉴스/news_txt /Volumes/Nas
+
+#upload previous
+
+python3 test.py --day 20200121
+python3 test.py --day 20200122
+python3 test.py --day 20200123
+python3 test.py --day 20200124
+python3 test.py --day 20200125
+python3 test.py --day 20200126
+python3 test.py --day 20200127
+python3 test.py --day 20200128
+python3 test.py --day 20200129
+python3 test.py --day 20200130
